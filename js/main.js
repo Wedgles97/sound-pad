7b6efc6eecbcab67a9547c0149b747b8a0ea9e54
Wedgles97 <wedegles@gmail.com>
2023-05-12 16:59:46 -0300

    adding js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // plays audio when clicking with the mouse
+let elementButton = document.querySelectorAll("button");
+
+elementButton.forEach((selectedAudio) => {
+  selectedAudio.addEventListener("click", () => {
+    let playAudio = document.querySelector(`audio[data-value="${selectedAudio.dataset.button}"]`);
+    document
+      .querySelectorAll("audio")
+      .forEach((selectedAudio) => selectedAudio.pause()); // When clicking on another audio, it interrupts the previous one
+      playAudio.currentTime = 0; // reset audio
+      playAudio.play();
+  });
+});
